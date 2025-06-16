93f2a033fc2319663f49a13a75bcda1913211877
Evgen Shevchenko <2058931@gmail.com>
2025-06-16 14:33:59 +0300

    test api 23

--- apicore/api/views.py
+++ apicore/api/views.py
@@ -12,7 +12,7 @@ from rest_framework import generics
 from rest_framework.authtoken.models import Token
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.authntication import TokenAuthentication
+from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated, AllowAny
 
 from urllib.parse import parse_qsl, unquote
